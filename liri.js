0124b7acd17b08bdac64ddc7c28d3dfdadc03297
Fabian Flores <ffflores1@outlook.com>
2018-04-09 17:20:01 -0400

    linted js files

--- liri.js
+++ liri.js
@@ -1,6 +1,8 @@
+var keys, spotify, client;
+
 require("dotenv").config();
 
-var keys = require("./keys.js");
+keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+spotify = new Spotify(keys.spotify);
+client = new Twitter(keys.twitter);
