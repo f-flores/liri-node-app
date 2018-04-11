5096cc2cb05e2419cfe86605b06038997c49aabb
Fabian Flores <ffflores1@outlook.com>
2018-04-10 20:36:20 -0400

    modified README.md

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # liri-node-app
 liri-node-app
+
+The liri-node-app is a terminal based app that processes commands like my-tweets.
+
+### Usage
+
+`node ./liri.js my-tweets` -- returns my most recent 20 tweets.
+
+### Installation
+
+This app can be cloned with the following command from a terminal. 
+
+However, in order to successfully run this app, a few programs must be already installed as prerequisites.
+
+1. git must be installed. 
+  [Download git.](https://git-scm.com/downloads)
+
+2. nodejs must also be installed.
+  [Download nodejs](https://nodejs.org/en/download/)
+
+3. Now we are ready to clone this app by running the following command.
+
+* `git clone git@github.com:f-flores/liri-node-app.git`
+
+4. Since this file makes use of various node modules, please run
+
+* npm install
+
+This installs all of the dependencies.
+
+5. This app utilizes the `twitter` and `spotify` npm packages, so to sucessfully run this app, please include your twitter and spotify keys in an `.env` file.
+
+SPOTIFY_ID=your_spotify_id
+SPOTIFY_SECRET=your_spotify_secret
+
+TWITTER_CONSUMER_KEY=your_twitter_consumer_key
+TWITTER_CONSUMER_SECRET=your_twitter_consumer_secret
+TWITTER_ACCESS_TOKEN_KEY=your_twitter_access_token_key
+TWITTER_ACCESS_TOKEN_SECRET=your_twitter_access_token_secret
+
+
+### Comments
+
+The Multi-RPS app was added to my github profile's portfolio:
+
+
+
--- liri.js
+++ liri.js
@@ -1,8 +1,57 @@
-var keys, spotify, client;
+// ===========================================================================================
+//  File name: liri.js
+//  Date: April, 2018
+//  Author: Fabian Flores
+//  Description: This app is a nodejs terminal app designed to process the following
+//    commands: mytweets
+//    mytweets returns a list of my 20 most recent tweets.
+//
+//
+// ===========================================================================================
+
+var keys, spotify, client, params;
+var appMethod;
+var Twitter;
 
 require("dotenv").config();
 
 keys = require("./keys.js");
+Twitter = require("twitter");
 
-spotify = new Spotify(keys.spotify);
+// spotify = new Spotify(keys.spotify);
 client = new Twitter(keys.twitter);
+
+appMethod = process.argv["2"];
+
+switch (appMethod) {
+  case "my-tweets":
+    myTweets();
+    break;
+  default:
+    console.log("Command not understood. Valid commands are 'my-tweets'");
+    break;
+}
+
+
+// ---------------------------------------------------------------------------------------
+// myTweets() outputs my last twenty tweets to my terminal window.
+//
+function myTweets() {
+  // params = {"screen_name": "nodejs"};
+  params = {"screen_name": "coding_ff"};
+
+  client.get("statuses/user_timeline", params, (error, tweets, response) => {
+    if (error) {
+      console.log("Error. Could not process twitter request.");
+
+      return false;
+    }
+
+    console.log(tweets);
+
+    return true;
+  });
+// client.get("search/tweets", {"q": "from @coding_ff"}, (error, tweets, response) => {
+//  console.log(tweets);
+// });
+}
